e65c3a3bd527195c1613d51d43c7ff2186300655
Bharat Ramanathan <bharat.ramanathan@wandb.com>
2025-02-05 13:51:09 +0800

    feat: add file upload and apikey verification

--- mods/labeling/pairwise/app.py
+++ mods/labeling/pairwise/app.py
@@ -1,7 +1,12 @@
+import csv
+import os
+
 import wandb
-from components import create_login_page
-from fasthtml.common import Link, Meta, Request, Script, fast_app, serve
+import weave
+from fasthtml.common import Link, Meta, Request, Script, fast_app, serve, Div
 from starlette.datastructures import FormData
+
+from components import create_login_page
 
 app, rt = fast_app(
     # live=True,
@@ -27,13 +32,44 @@ def get():
 
 
 def validate_wandb(entity: str, project: str, api_key: str):
-    _api = wandb.Api(api_key=api_key, overrides={"entity": entity, "project": project})
+    try:
+        logged_in = wandb.login(key=api_key, verify=True)
+
+    except ValueError as e:
+        raise e
+    return logged_in
+
+
+async def parse_file(file_obj):
+    ds_name = os.path.splitext(file_obj.filename)[0]
+    file_contents = await file_obj.read()
+    file_contents = file_contents.decode("utf-8").splitlines()
+    reader = csv.DictReader(file_contents)
+    assert (
+        len(reader.fieldnames) <= 3
+    ), "Only the following fields are supported: 'Prompt', 'Model A', 'Model B'"
+    dataset = [line for line in reader]
+    return ds_name, dataset
 
 
 @rt("/upload", methods=["POST"])
 async def post(request: Request):
     contents: FormData = await request.form()
-    print(contents)
+    try:
+        valid_key = validate_wandb(
+            entity=contents["wandb_entity"],
+            project=contents["wandb_project"],
+            api_key=contents["wandb_key"],
+        )
+        if valid_key:
+            _ = weave.init(f'{contents["wandb_entity"]}/{contents["wandb_project"]}')
+            ds_name, dataset = await parse_file(contents["FileUpload"])
+            ws_ds = weave.Dataset(name=ds_name, rows=dataset)
+            ds_obj_ref = weave.publish(ws_ds)
+            # TODO: Make this a more polished div
+            return Div(f"Raw Weave Dataset Published at {ds_obj_ref.uri()}")
+    except ValueError as e:
+        return str(e)
 
     return "ok"
 
